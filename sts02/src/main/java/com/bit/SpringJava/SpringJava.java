eb9feae8b0859449f88fac400e07d7dbe05a7082
Deok Soo <dsx2____@nate.com>
2020-02-17 10:33:33 +0900

    spring java(DI & IoC)

---
+++ sts02/src/main/java/com/bit/SpringJava/SpringJava.java
@@ -0,0 +1,15 @@
+package com.bit.SpringJava;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bit.service.Remote;
+
+public class SpringJava {
+
+	public static void main(String[] args) {
+		org.springframework.context.ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Remote obj=(Remote)ac.getBean("remote");
+		obj.func3();
+	}
+
+}
---
+++ sts02/src/main/java/com/bit/service/Machine.java
@@ -0,0 +1,9 @@
+package com.bit.service;
+
+public interface Machine {
+	
+	void on();
+	void work();
+	void off();
+
+}
